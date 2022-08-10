965cd3bf72a07a5868eaca8dc8e02d0e12933583
Gui-pin <guilhermepinheiro082005@gmail.com>
2022-08-10 16:00:29 -0500

    #3 Permitir que o usuário selecione a opção que deseja

--- calculadora.py
+++ calculadora.py
@@ -1,10 +1,20 @@
-number_1 = int(input('Enter your first number: '))
-number_2 = int(input('Enter your second number: '))
+operation = input('''Plase type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+\n''')
 
-print('{} + {} = {}'.format(number_1, number_2, number_1+number_2))
+number_1 = int(input('\nEnter your first number: '))
+number_2 = int(input('\nEnter your second number: '))
 
-print('{} - {} = {}'.format(number_1, number_2, number_1-number_2))
-
-print('{} * {} = {}'.format(number_1, number_2, number_1*number_2))
-
-print('{} / {} = {}'.format(number_1, number_2, number_1/number_2))
+if operation == '+':
+    print('\n{} + {} = {}'.format(number_1, number_2, number_1+number_2))
+elif operation == '-':
+    print('\n{} - {} = {}'.format(number_1, number_2, number_1-number_2))
+elif operation == '*':
+    print('\n{} * {} = {}'.format(number_1, number_2, number_1*number_2))
+elif operation == '/':
+    print('\n{} / {} = {}'.format(number_1, number_2, number_1/number_2))
+else:
+    print('You have not typed a valid operator, please run the program again.')
